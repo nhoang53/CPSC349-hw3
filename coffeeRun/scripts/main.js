3d738f2398aef8087897653eef396cd63d0a3399
nhoang53 <he_llo1002@yahoo.com>
2019-09-24 18:51:15 -0700

    payment

--- coffeeRun/scripts/main.js
+++ coffeeRun/scripts/main.js
@@ -2,10 +2,12 @@
   'use strict';
   var FORM_SELECTOR = '[data-coffee-order="form"]';
   var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+  //var PAYMENT_SELETOR = '[data-coffee-payment="payment"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  //var Payment = App.Payment;
   var myTruck = new Truck('ncc-1701', new DataStore());
   window.myTruck = myTruck; // export it as global namespace
 
